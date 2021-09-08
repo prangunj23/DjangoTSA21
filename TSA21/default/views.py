01f2447a971732d08c3496848cc480a133e201e2
Pranit <prangunj23@ncs.charter.k12.de.us>
2021-09-07 21:10:34 -0400

    Code

--- TSA21/default/views.py
+++ TSA21/default/views.py
@@ -2,6 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, "default/index.html", {
-        "hello": "Hello World"
-    })
+    return render(request, "default/index.html", {})

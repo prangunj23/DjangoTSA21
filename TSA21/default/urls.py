415076be88f975ab838c14a9a641ed6c5074c678
Pranit <prangunj23@ncs.charter.k12.de.us>
2021-10-08 13:23:58 -0400

    Code

--- TSA21/default/urls.py
+++ TSA21/default/urls.py
@@ -2,8 +2,11 @@ from django.urls import path
 
 from . import views
 
+
 urlpatterns = [
     path('', views.index, name='index'),
     path('main', views.main, name='main'),
-    path('login', views.login, name='login'),
+    path('login', views.loginuser, name='login'),
+    path('register', views.register, name = 'register'),
+    path('logout', views.logoutuser, name='logout')
 ]
--- TSA21/default/views.py
+++ TSA21/default/views.py
@@ -1,17 +1,35 @@
-from django.shortcuts import render
-from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout 
+from django.contrib.auth.forms import UserCreationForm
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 
+
 # Create your views here.
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('login')
+
+    else:
+        
+            form = UserCreationForm()
+    return render(request, "default/register.html", {'form': form})
+
 def index(request):
     return render(request, "default/index.html", {})
-
+ 
 def main(request):
     if not request.user.is_authenticated:
         return HttpResponseRedirect(reverse("login"))
     return render(request, "default/user.html")
-def login(request):
+def loginuser(request):
     if request.method == "POST":
         username = request.POST["username"]
         password = request.POST["password"]
@@ -25,5 +43,9 @@ def login(request):
             })
     return render(request, "default/login.html")
 
-def logout(request):
-    pass
+def logoutuser(request):
+    logout(request)
+    return render(request, "default/login.html", {
+        "message": "Logged out."
+    })
+

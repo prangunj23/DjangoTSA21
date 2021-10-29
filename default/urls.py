40581d1d5db5dce9e91e9e9af4c532c40f72c3c6
unknown <pranit.gunjal@ncs.local>
2021-10-29 08:29:00 -0400

    added some abstractions around events db

--- default/urls.py
+++ default/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('login', views.loginuser, name='login'),
     path('register', views.register, name = 'register'),
     path('logout', views.logoutuser, name='logout'),
-    path('user', views.user, name="user")
+    path('user', views.user, name="user"),
+    path('event', views.event, name="event")
 ]

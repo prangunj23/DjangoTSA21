0c2916b8478987ce196aa68a6b0988338f1462ab
Pranit Gunjal <96096788+aarugunj27@users.noreply.github.com>
2022-02-02 20:47:03 -0500

    updated 404

--- TSA21/urls.py
+++ TSA21/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('default.urls'))
 ]
+
+handler404 = "default.views.handler404"

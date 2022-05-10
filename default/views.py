910ab13438126527676e6b2135b97e120cb37d6b
aarugunj27 <96096788+aarugunj27@users.noreply.github.com>
2022-05-09 21:55:19 -0400

    Update views.py

--- default/views.py
+++ default/views.py
@@ -17,14 +17,14 @@ def register(request):
         form = UserCreationForm(request.POST)
         if form.is_valid():
             form.save()
-            username = form.cleaned_data.get('username')
-            raw_password = form.cleaned_data.get('password1')
-            user = authenticate(username=username, password=raw_password)
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
             login(request, user)
+            messages.success(request, ("Registration Successful!"))
             return HttpResponseRedirect(reverse("index"))
 
     else:
-
             form = UserCreationForm()
     return render(request, "default/register.html", {'form': form})
 

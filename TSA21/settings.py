e933ba4cdea569226edfaeb973819df6c4587910
prangunj23 <65430428+prangunj23@users.noreply.github.com>
2022-06-21 19:20:09 -0400

    Update settings.py

--- TSA21/settings.py
+++ TSA21/settings.py
@@ -136,5 +136,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'virchess123@gmail.com'
-EMAIL_HOST_PASSWORD = 'esfdvwuvatapexnb'
+EMAIL_HOST_PASSWORD = 'kqui irmr khyq xtoq'
 EMAIL_USE_TLS = True

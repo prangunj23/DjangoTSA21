feb043f94588c1803f242bf7dc580b8b588d5380
prangunj23 <65430428+prangunj23@users.noreply.github.com>
2022-06-22 20:33:41 -0400

    Update forms.py

--- default/forms.py
+++ default/forms.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.forms import UserCreationForms
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django import Forms
 

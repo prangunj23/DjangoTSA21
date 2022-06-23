46d1c85b245b49c8b3fb57ff718114a82c5fea80
prangunj23 <65430428+prangunj23@users.noreply.github.com>
2022-06-22 20:36:12 -0400

    Update forms.py

--- default/forms.py
+++ default/forms.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from django import Forms
+from django import forms
 
 class RegisterUserForm(UserCreationForm):
   
